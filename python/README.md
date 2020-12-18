4fe4e823d1331db9c493857ca3a258bbeb39797a
Jared <jarednkrause@gmail.com>
2020-12-18 18:05:45 -0500

    readme

---
+++ python/README.md
@@ -0,0 +1,13 @@
+# fullfillment api
+
+Wanted to see if I could brush up on my python well enough to solve this again.
+
+## Get Started:
+
+1. Install python
+2. Run `python main.py`
+
+## Known Issues
+* Restocking causes problems with editing items.
+* Needs a proper logging service to give some more details.
+* All the issues in the javascript app are present here.
--- python/src/main.py
+++ python/src/main.py
@@ -1,9 +1,10 @@
 import inventory_service
-inventory = inventory_service.inventory_service()  # get an instance of the class
 import order_service
+import json
+
+inventory = inventory_service.inventory_service()
 orders = order_service.order_service()
 
-import json
 sample = json.load(open('inventory.json', 'r'))
 restock = json.load(open('restock.json', 'r'))
 order = json.load(open('order.json', 'r'))
@@ -12,6 +13,5 @@ def init():
   inventory.init_catalogue(sample)
   inventory.process_restock(restock)
   orders.process_orders(order)
-  # inventory.process_restock(restock)
 
 init()
